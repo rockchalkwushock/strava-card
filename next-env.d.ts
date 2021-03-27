0d46e98397d96e0b57c23891944390c2d03351e2
Cody Brunner <rockchalkwushock@icloud.com>
2021-03-27 12:09:18 -0500

    chore(root): adds dependencies and envvars.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    // Node Environment Variables
+    NODE_ENV: 'development' | 'production' | 'test'
+    // Custom Environment Variables
+    NEXT_PUBLIC_STRAVA_ATHLETE_ID: string
+    STRAVA_CLIENT_ID: string
+    STRAVA_CLIENT_SECRET: string
+    STRAVA_REFRESH_TOKEN: string
+  }
+}
